ab3d94a06b018337fb519e135dcc402c2a189d5b
Maya <maya.atiah.99@outlook.com>
2023-01-03 22:47:16 +0200

    step3

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
 
+//const for time
+const today= new Date();
+const time= today.getHours() + ":" + today.getMinutes();
 
-app.get('/',(req,res)=>{
+//answers ok step2
+app.get('/', (req, res) => {
     res.send('ok');
 });
+
+// url "/test" step3
+app.get('/test', (req, res) => {
+    res.send({ status: 200, message: "ok" });
+});
+
+//url "/time" step3
+app.get('/time',(req,res)=>{
+    res.send({status:200, message:time})
+})
 
 app.listen(3000, () => {
     console.log("Example app listening on 3000...")
 })
+
+//const port=process.env.PORT || 3000
+//app.listen(port,()=>console.log(`listen ${port}`))
+//export Port=5000
