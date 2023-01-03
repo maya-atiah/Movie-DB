e963f060839faf3f6b0d60b94b1dce57c76a581c
Maya <maya.atiah.99@outlook.com>
2023-01-03 22:06:47 +0200

    step 2

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.send('ok');
+});
+
+app.listen(3000, () => {
+    console.log("Example app listening on 3000...")
+})
